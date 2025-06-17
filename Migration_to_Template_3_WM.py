5bfb72c6b38c306209caed51ce9ad8385d3c23d6
6631503001 <koonnuthhh@gmail.com>
2025-06-18 01:59:45 +0700

    Upgrade WM 3 to align with new requirement
    -Add function to clean column which use pandas, column name, array of mapping
    -Add logic to clean reg_date in WM 3

--- Migration_to_Template_3_WM.py
+++ Migration_to_Template_3_WM.py
@@ -6,10 +6,20 @@ from function.Function_Plate_Provice.plate_code import prepare
 from function.clean_column_values import clean_column_names
 
 output_file = "Template_3_WM_output.xlsx"
-clean_column_values = {
+def keep_valid_float(val):
+    try:
+        # Strip leading/trailing spaces and normalize value
+        val_str = str(val).strip().replace(",", "")
+        float_val = float(val_str)
+        return float_val if float_val != 0.0 else "-"
+    except (ValueError, TypeError):
+        return "-"
+
+def Migration_to_Template_3_WM(source_file,destination_file,source_sheet,destination_sheet) :
+    clean_column_values = {
     "MITSUBISHI": ["Mitsubihi", "Mitsubshi", "Misubishi", "Mitsubashi", "Mitzubishi"],
     "MG": ["Mg", "mg", "MGG", "MG.", "M-G"],
-    "TOYOTA": ["Toyata", "Totota", "Toyoya", "Toyoya", "T0yota"],
+    "TOYOTA": ["Toyata", "Totota", "Toyoya", "Toyoya", "T0yota","TOTOYA","TOYOYA","TOYATA"],
     "ISUZU": ["Iszuzu", "Isuzu", "Isuzu", "Iszsu", "Izuzu"],
     "DFSK": ["DFK", "DFSKK", "D-FSK", "DSFK", "DFFSK"],
     "CHEVROLET": ["Chevorlet", "Chevy", "Chevrolat", "Chev", "Cheverlot"],
@@ -25,16 +35,6 @@ clean_column_values = {
     "YAMAHA": ["Yamha", "Yamama", "Yamhaa", "Yamah", "Yamhaa", "YAMAYA"],
     "VESPA": ["Vesbaa", "Vesspa", "Vspa", "Veespa", "Vesap"],
 }
-def keep_valid_float(val):
-    try:
-        # Strip leading/trailing spaces and normalize value
-        val_str = str(val).strip().replace(",", "")
-        float_val = float(val_str)
-        return float_val if float_val != 0.0 else "-"
-    except (ValueError, TypeError):
-        return "-"
-
-def Migration_to_Template_3_WM(source_file,destination_file,source_sheet,destination_sheet) :
 
     column_mapping = {
         'เลขที่สัญญา': 'เลขที่สัญญา',
@@ -51,12 +51,12 @@ def Migration_to_Template_3_WM(source_file,destination_file,source_sheet,destina
 
     template_df = map_excel_columns(source_file,destination_file,source_sheet,destination_sheet,column_mapping)
     template_df  = template_df[template_df['เลขที่สัญญา'].astype(str).str.startswith("48")]
-    model_code_df = clean_column_names(template_df,"brand_code",column_mapping)
-    template_df["brand_code"] = model_code_df["model_code"]
+    model_code_df = clean_column_names(template_df,"brand_code",clean_column_values)
+    #template_df["brand_code"] = model_code_df["model_code"]
     template_df['reg_date'] = template_df['reg_date'].astype(str).apply(
     lambda x: x if x.isdigit() and 1900 <= int(x) <= 2100 else '-'
     )
-    template_df.drop(columns=["model_code"], inplace=True)
+    #template_df.drop(columns=["model_code"], inplace=True)
     template_df['rate_book'] = template_df['rate_book'].apply(keep_valid_float)
     
     prepare(template_df)
